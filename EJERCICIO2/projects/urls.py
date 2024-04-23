627406ea0c632bbc865720a24acae90a65adfc7e
SofiaRebolledo <laurarebolledo@usantotomas.edu.co>
2024-04-22 22:36:25 -0500

    login y registro

--- EJERCICIO2/projects/urls.py
+++ EJERCICIO2/projects/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('projects/', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
+    path('login/', views.login, name="login"),
 ]
--- EJERCICIO2/projects/views.py
+++ EJERCICIO2/projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+def projects(request):
+    return render(request, 'Plantilla.html')
+
+def project(request, pk):
+    return render(request, 'oooooooooo.html')
+
+def login(request):
+    return render(request,'registro.html')
