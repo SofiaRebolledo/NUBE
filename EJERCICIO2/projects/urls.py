22ce730fa8610938a5b6645b2fcce6abb3078339
SofiaRebolledo <83147068+SofiaRebolledo@users.noreply.github.com>
2024-04-22 22:58:20 -0500

    Delete EJERCICIO2 directory

--- EJERCICIO2/projects/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('projects/', views.projects, name="projects"),
-    path('project/<str:pk>/', views.project, name="project"),
-    path('login/', views.login, name="login"),
-]
