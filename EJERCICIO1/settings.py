d24b9727ac342f477e839339363d0ec1814b7cff
SofiaRebolledo <laurarebolledo@usantotomas.edu.co>
2024-04-28 23:14:21 -0500

    navbar y url en todo

--- EJERCICIO1/settings.py
+++ EJERCICIO1/settings.py
@@ -74,24 +74,24 @@ WSGI_APPLICATION = 'EJERCICIO1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'projects',
-        'USER' : 'postgres',
-        'PASSWORD' : 'admin',
-        'HOST' : 'localhost',
-        'PORT' : 5432,
-    }
-}
-
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'projects',
+#        'USER' : 'postgres',
+#        'PASSWORD' : 'admin',
+#        'HOST' : 'localhost',
+#        'PORT' : 5432,
 #    }
 #}
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
