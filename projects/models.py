292643500b90c7abbbd98d253e9aac67652b4569
Nico2402 <nicolascedeno@usantotomas.edu.co>
2024-04-28 19:52:45 -0500

    Models y postgreSQL

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,22 @@
+import email
+import uuid
+from email.policy import default
+from enum import unique
+from operator import truediv
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+    
+class Usuario(models.Model):
+    documento = models.UUIDField(default=uuid.uuid4,unique=True, primary_key=True,editable=False)
+    nombre = models.CharField(max_length=200)
+    email = models.TextField(null=True,blank=True)
+
+class Analista(models.Model):
+    document = models.UUIDField(default=uuid.uuid4,unique=True, primary_key=True,editable=False)
+    nombre = models.CharField(max_length=200)
+    email = models.TextField(null=True,blank=True)
+
+def _str_(self):
+    return self.name
