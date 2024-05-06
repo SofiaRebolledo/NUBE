4c7f9eda2ffa56df3cd3bb637e177c7742eefc3a
Nico2402 <nicolascedeno@usantotomas.edu.co>
2024-05-06 12:13:27 -0500

    Creacion de usuarios

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+import psycopg2
 
+conn = psycopg2.connect(
+    dbname="projects",
+    user="postgres",
+    password="admin",
+    host="localhost",
+    port ="5432"
+)
+cur = conn.cursor()
+
+@login_required
 def inicio(request):
     return render(request, 'Plantilla.html')
 
@@ -8,7 +21,34 @@ def login(request):
     return render(request,'login.html')
 
 def registro(request):
+    rol = 3
+    if request.method=='POST':
+        nombre =  str((request.POST.get('nombre'))).strip(' ')
+        apellido = str(request.POST.get('apellido')).strip(' ')
+        telefono= int(request.POST.get('telefono'))
+        cedula= int(request.POST.get('cedula'))
+        email= str(request.POST.get('email')).strip(' ')
+        password= str(request.POST.get('password'))
+        username = nombre + "_" + apellido + "_" + str(cedula).strip(" ")
+        my_user=User.objects.create_user(username,email,password)
+        my_user.save()
+
+
+        #query = "update public.auth_user set idrol = "+ str(rol) +" where email = '"+ email + "'"
+        #print(query)
+        cur.execute("update public.auth_user set idrol = "+ str(rol) +" where username = '"+ username + "'")
+        filas = None
+        fila = None
+        cur.execute("select idrol from public.auth_user")
+        filas = cur.fetchall()
+        for fila in filas:
+            print(fila)
+        
+        conn.commit()
+
+        print("Username:  "+username,nombre,apellido,telefono,cedula,email,password)
     return render(request,'registro.html')
+    
 
 def forgot_password(request):
     return render(request,'forgot_password.html')
