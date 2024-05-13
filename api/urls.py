f49d65e64a9cc11f49abc962c5c008b3342d5492
Nico2402 <92763093+Nico2402@users.noreply.github.com>
2024-05-13 01:28:00 -0500

    Delete api directory

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from api import views
-
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('get_chart_historico/',views.getcharthistorico,name='getcharthistorico'),
-    path('get_chart_indices/',views.getchartindices,name='getchartindices'),
-    path('get_chart_tendencia/',views.getcharttendencia,name='getcharttendencia')
-]
